8ac6ba1afc1f9a953eea4fe3c6921130ea176d86
luciacantos <luciacantos22@gmail.com>
2022-12-12 23:07:28 +0100

    pedir casilla de destino

--- Modulos/movimientos.py
+++ Modulos/movimientos.py
@@ -14,4 +14,8 @@ def movimientos(tablero):
             print("Ingrese la posición de la ficha que quiere mover.")
             fila_o = int(input("Fila: ")) #fila de origen de la ficha que mueve
             columna_o = int(input("Columna: "))#columna de origen de la ficha que mueve
+            print("Ingrese la posición de la casilla a la que quiere mover la ficha. ")
+            fila_d = int(input("Fila: "))#fila destino de la ficha
+            columna_d = int(input("Columna: "))#columna destino de la ficha
+            print(" ")
 
