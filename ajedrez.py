a9ac56b32af32b73b67dccdb0c5c8a848adc5bf6
luciacantos <luciacantos22@gmail.com>
2022-12-12 23:39:49 +0100

    corrección

--- ajedrez.py
+++ ajedrez.py
@@ -5,21 +5,21 @@ peones_negros = ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟']
 fichas_blancas = ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']
 peones_blancos = ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙']
 
-tablero_inicial = [fichas_negras, peones_negros,[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], peones_blancos, fichas_blancas]
+tablero = [fichas_negras, peones_negros,[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], peones_blancos, fichas_blancas]
 
-def imprimir_tablero_inicial(tablero_inicial):
+def imprimir_tablero(tablero):
     print("    1 2 3 4 5 6 7 8")
     print(" ")
     for i in range(8):
         print(i+1, end='  ')
         for j in range(8):
-            print(tablero_inicial[i][j], end=' ')
+            print(tablero[i][j], end=' ')
         print()
 
 if __name__ == '__main__':
-    imprimir_tablero_inicial(tablero_inicial)
+    imprimir_tablero(tablero)
 
-def movimientos(tablero_inicial):
+def movimientos(tablero):
 
     movimiento = 0
 
@@ -34,12 +34,12 @@ def movimientos(tablero_inicial):
             fila_d = int(input("Fila: "))
             columna_d = int(input("Columna: "))
             print(" ")
-            tablero_inicial[fila_d - 1][columna_d - 1] = tablero_inicial[fila_o - 1][columna_o - 1]
-            tablero_inicial[fila_o - 1][columna_o - 1] = " "
+            tablero[fila_d - 1][columna_d - 1] = tablero[fila_o - 1][columna_o - 1]
+            tablero[fila_o - 1][columna_o - 1] = " "
 
             movimiento = movimiento + 1
 
-            print(imprimir_tablero_inicial(tablero_inicial))
+            print(imprimir_tablero(tablero))
             print("Movimiento N°", movimiento)
         elif respuesta == "2":
             print("El tablero se quedará igual.")
@@ -49,4 +49,4 @@ def movimientos(tablero_inicial):
             print("ingrese una opcion valida")
 
 if __name__ == '__main__':
-    movimientos(tablero_inicial)
+    movimientos(tablero)
