09469fcdc1f96e5bb68b7792fe8cb48caff95b48
luciacantos <luciacantos22@gmail.com>
2022-12-12 23:05:49 +0100

    importar módulos

--- Modulos/tablero_inicial.py
+++ Modulos/tablero_inicial.py
@@ -1,10 +1,12 @@
 # importar fichas.py y usar las variables
 
-from data.fichas import *
+from data.fichas import fichas_negras, peones_negros, fichas_blancas, peones_blancos
 
+#tablero inicial
 tablero = [fichas_negras, peones_negros,[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], peones_blancos, fichas_blancas]
 
-def imprimir_tablero_inicial(tablero):
+#imprimir por pantalla con forma de tablero y los numeros de las filas y columnas
+def imprimir_tablero(tablero):
     print("    1 2 3 4 5 6 7 8")
     print(" ")
     for i in range(8):
